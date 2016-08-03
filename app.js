3f16de9a326fad2d94805349ee04bce9cb4cd4a3
Mark Eads <meads@hcpci.com>
2016-08-03 14:50:46 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+
+var app = express();
+
+var port = 1987;
+
+//middleware
+app.use(express.static('public'));
+app.use(express.static('source/views'));
+
+app.get('/', function(req, res) { 
+    res.send('hello world');
+});
+
+app.get('/books', function(req, res) { 
+    res.send('hello books');
+});
+
+
+app.listen(1987, function(err)  {
+    console.log('running server on port: ' + port);
+})
